d096a223d5376702a093205dce01b4b135d1725a
Rakesh <rakeshkumar8900@gmail.com>
2019-03-15 16:06:07 +0530

    title added (#2)

--- components/examples.js
+++ components/examples.js
@@ -5,6 +5,7 @@ import Counter from './counter'
 function Examples ({ lastUpdate, light }) {
   return (
     <div>
+      <h3>Kickstarter for Reactjs,Redux with server side rendering via Nextjs</h3>
       <Clock lastUpdate={lastUpdate} light={light} />
       <Counter />
     </div>
